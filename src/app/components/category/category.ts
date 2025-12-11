eb781492486325f463088ed3ede126be3d7562b1
nubinub <fab_thomas@msn.com>
2025-12-11 16:23:18 +0100

    feat: Add Group Pill to Category

--- src/app/components/category/category.ts
+++ src/app/components/category/category.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { ICategory } from '../../models/categories';
+import { PillComponent } from '../pill/pill';
 
 @Component({
   selector: 'cvw-category',
-  imports: [],
+  imports: [PillComponent],
   templateUrl: './category.html',
 })
 export class CategoryComponent {
