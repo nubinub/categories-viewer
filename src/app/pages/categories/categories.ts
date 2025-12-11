42a652a63a5f62fc08820e0510110b3ecb230473
nubinub <fab_thomas@msn.com>
2025-12-11 15:49:37 +0100

    feat: categories are displayed inside a Card component

--- src/app/pages/categories/categories.ts
+++ src/app/pages/categories/categories.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CardComponent } from '../../components/card/card';
 import { CategoryComponent } from '../../components/category/category';
 import { CategoryLogic } from '../../services/logic/category-logic/category-logic';
 
 @Component({
   selector: 'cvw-categories',
-  imports: [CategoryComponent, CommonModule],
+  imports: [CardComponent, CategoryComponent, CommonModule, CardComponent],
   templateUrl: './categories.html',
 })
 export class Categories {
